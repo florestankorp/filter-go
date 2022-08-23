90e7fa0d32b263d568adb87926bf99562e687802
Florestan Korp <florestankorp@gmail.com>
2022-08-23 12:05:59 +0200

    added error handling for unsupported file format

--- main.go
+++ main.go
@@ -26,4 +26,14 @@ func main() {
 	var bitmapInfoHeader bmp.BITMAPINFOHEADER
 	binary.Read(buffer, binary.LittleEndian, &bitmapInfoHeader)
 
+	if bitmapFileHeader.BfType != 0x4d42 ||
+		bitmapFileHeader.BfOffBits != 54 ||
+		bitmapInfoHeader.BiSize != 40 ||
+		bitmapInfoHeader.BiBitCount != 24 ||
+		bitmapInfoHeader.BiCompression != 0 {
+
+		panic("Unsupported file format.\n")
+
+	}
+
 }
