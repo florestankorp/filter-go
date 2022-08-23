3aafedc5de3adc16ce6197b02d142a039f2a22a2
Florestan Korp <florestankorp@gmail.com>
2022-08-23 18:19:55 +0200

    [wip]

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"filter-go/bmp"
 	"fmt"
 	"os"
-	"unsafe"
 )
 
 func check(e error) {
@@ -34,13 +34,34 @@ func main() {
 		panic("Unsupported file format.\n")
 	}
 
-	height := -bitmapInfoHeader.Height // height is negative
-	width := bitmapInfoHeader.Width
+	// height := -bitmapInfoHeader.Height // height is negative
+	// width := bitmapInfoHeader.Width
 
-	var rgbTriple bmp.RGBTriple
-	padding := (4 - (int(width)*int(unsafe.Sizeof(rgbTriple)))%4) % 4
+	// var rgbTriple bmp.RGBTriple
+	// var padding int64 = (4 - (int64(width)*int64(unsafe.Sizeof(rgbTriple)))%4) % 4
 
-	fmt.Println(height)
-	fmt.Println(padding)
+	// allocate memory for pixels
+	// pixels := make([]bmp.RGBTriple, (int(height) * (int(width) * int(unsafe.Sizeof(rgbTriple)))))
+	r4 := bufio.NewReader(file)
+	b4, err := r4.Peek(5)
+	check(err)
+	fmt.Println(b4)
+	// for i, _ := range pixels {
+	// 	buffer := make([]byte, int(unsafe.Sizeof(rgbTriple)))
+	// 	file.Read(buffer)
+	// 	binary.Read(bytes.NewReader(buffer), binary.LittleEndian, &pixels[i])
+
+	// }
+
+	// iterate over scanlines
+	// for i := 0; i < int(height); i++ {
+	// 	buffer := make([]byte, int(width)*int(unsafe.Sizeof(rgbTriple)))
+	// 	file.Read(buffer)
+	// 	binary.Read(bytes.NewReader(buffer), binary.LittleEndian, &pixels[i])
+
+	// 	file.Seek(padding, 0)
+	// }
+
+	// fmt.Println(pixels[0])
 
 }
