f84d0633c3bb7029cff58e27d4263539cf53f267
Florestan Korp <florestankorp@gmail.com>
2022-08-23 12:58:22 +0200

    removed type aliases from structs

--- bmp/bmp.go
+++ bmp/bmp.go
@@ -1,10 +1,5 @@
 package bmp
 
-type BYTE uint8
-type DWORD uint32
-type LONG int32
-type WORD uint16
-
 /*
 RGBTRIPLE
 
@@ -27,11 +22,11 @@ and layout of a file that contains a DIB [device-independent bitmap].
 Adapted from http://msdn.microsoft.com/en-us/library/dd183374(VS.85).aspx.
 */
 type BITMAPFILEHEADER struct {
-	BfType      WORD
-	BfSize      DWORD
-	BfReserved1 WORD
-	BfReserved2 WORD
-	BfOffBits   DWORD
+	BfType      uint16
+	BfSize      uint32
+	BfReserved1 uint16
+	BfReserved2 uint16
+	BfOffBits   uint32
 }
 
 /*
@@ -43,15 +38,15 @@ dimensions and color format of a DIB [device-independent bitmap].
 Adapted from http://msdn.microsoft.com/en-us/library/dd183376(VS.85).aspx.
 */
 type BITMAPINFOHEADER struct {
-	BiSize          DWORD
-	BiWidth         LONG
-	BiHeight        LONG
-	BiPlanes        WORD
-	BiBitCount      WORD
-	BiCompression   DWORD
-	BiSizeImage     DWORD
-	BiXPelsPerMeter LONG
-	BiYPelsPerMeter LONG
-	BiClrUsed       DWORD
-	BiClrImportant  DWORD
+	BiSize          uint32
+	BiWidth         int32
+	BiHeight        int32
+	BiPlanes        uint16
+	BiBitCount      uint16
+	BiCompression   uint32
+	BiSizeImage     uint32
+	BiXPelsPerMeter int32
+	BiYPelsPerMeter int32
+	BiClrUsed       uint32
+	BiClrImportant  uint32
 }
