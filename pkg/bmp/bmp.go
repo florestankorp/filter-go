7a6f976f3393a80025e7ccdf4995fae0b53390af
Florestan Korp <florestankorp@gmail.com>
2022-08-24 23:12:29 +0200

    changed folder structure + refactor

--- pkg/bmp/bmp.go
+++ pkg/bmp/bmp.go
@@ -3,6 +3,7 @@ package bmp
 import (
 	"bytes"
 	"encoding/binary"
+	"filter-go/pkg/utils"
 	"fmt"
 	"os"
 )
@@ -22,13 +23,13 @@ type Pixel struct {
 }
 
 /*
-BitmapFileHeader
+FileHeader
 
-The BitmapFileHeader structure contains information about the type, size,
+The FileHeader structure contains information about the type, size,
 and layout of a file that contains a DIB [device-independent bitmap].
 Adapted from http://msdn.microsoft.com/en-us/library/dd183374(VS.85).aspx.
 */
-type BitmapFileHeader struct {
+type FileHeader struct {
 	Type      uint16
 	Size      uint32
 	Reserved1 uint16
@@ -37,14 +38,14 @@ type BitmapFileHeader struct {
 }
 
 /*
-BitmapInfoHeader
+InfoHeader
 
-The BitmapInfoHeader structure contains information about the
+The InfoHeader structure contains information about the
 dimensions and color format of a DIB [device-independent bitmap].
 
 Adapted from http://msdn.microsoft.com/en-us/library/dd183376(VS.85).aspx.
 */
-type BitmapInfoHeader struct {
+type InfoHeader struct {
 	Size          uint32
 	Width         int32
 	Height        int32
@@ -60,11 +61,55 @@ type BitmapInfoHeader struct {
 
 func EncodeHeader(bufferSize int, file *os.File, header interface{}) error {
 	buffer := make([]byte, bufferSize)
-	file.Read(buffer)
+	_, err := file.Read(buffer)
+	utils.Check(err)
 
 	if error := binary.Read(bytes.NewReader(buffer), binary.LittleEndian, header); error != nil {
 		return fmt.Errorf("failed to parse DIB header: %w", error)
 	}
 
 	return nil
+}
+
+func Grayscale(pixels *[]Pixel) {
+	for i := range *pixels {
+		avg := (int((*pixels)[i].Red) + int((*pixels)[i].Green) + int((*pixels)[i].Blue)) / 3
+
+		(*pixels)[i].Red = byte(avg)
+		(*pixels)[i].Green = byte(avg)
+		(*pixels)[i].Blue = byte(avg)
+	}
+
+}
+
+// Reflect image horizontally
+func Reflect(width int, height int, pixels *[]Pixel) {
+	buffer := make([]Pixel, 0, width*height)
+
+	for i := 0; i < height; i++ {
+		start := i * width
+		end := width + (i * width)
+		slice := (*pixels)[start:end]
+		buffer = append(buffer, reverse(slice)...)
+	}
+
+	*pixels = buffer
+}
+
+// // Blur image
+// func blur(height int, width  int, pixel image[height][width]){
+//     return;
+// }
+
+// // Detect edges
+// func edges(height int, width  int, pixel image[height][width]){
+//     return;
+// }
+
+func reverse(pixels []Pixel) []Pixel {
+	for i, j := 0, len(pixels)-1; i < j; i, j = i+1, j-1 {
+		pixels[i], pixels[j] = pixels[j], pixels[i]
+	}
+
+	return pixels
 }
